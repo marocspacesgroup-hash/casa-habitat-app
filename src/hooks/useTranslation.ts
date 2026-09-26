5c84846e112e035d8c738987eb29e5ca68ccda3a
marocspacesgroup-hash <marocspacesgroup@gmail.com>
2026-09-26 16:56:16 +0100

    feat(i18n): derive client translations from locale URL

--- src/hooks/useTranslation.ts
+++ src/hooks/useTranslation.ts
@@ -1,16 +1,25 @@
 "use client";
 
 import { startTransition, useEffect, useState } from "react";
+import { usePathname } from "next/navigation";
 import { isLanguage, translations, type Language } from "@/locales";
+import { getLocaleFromPath } from "@/lib/i18n/config";
 
 const STORAGE_KEY = "casa-habitat-language";
 
 export function useTranslation() {
-  const [language, setLanguage] = useState<Language>("fr");
+  const pathname = usePathname();
+  const routeLocale = getLocaleFromPath(pathname);
+  const [language, setLanguage] = useState<Language>(routeLocale ?? "fr");
 
   useEffect(() => {
-    const savedLanguage = window.localStorage.getItem(STORAGE_KEY);
-    if (isLanguage(savedLanguage)) startTransition(() => setLanguage(savedLanguage));
+    const pathLocale = getLocaleFromPath(window.location.pathname);
+    if (pathLocale) {
+      startTransition(() => setLanguage(pathLocale));
+    } else {
+      const savedLanguage = window.localStorage.getItem(STORAGE_KEY);
+      if (isLanguage(savedLanguage)) startTransition(() => setLanguage(savedLanguage));
+    }
 
     const onLanguageChange = (event: Event) => {
       const nextLanguage = (event as CustomEvent<string>).detail;
@@ -19,6 +28,12 @@ export function useTranslation() {
     window.addEventListener("casa-habitat-language-change", onLanguageChange);
     return () => window.removeEventListener("casa-habitat-language-change", onLanguageChange);
   }, []);
+
+  useEffect(() => {
+    if (routeLocale && routeLocale !== language) {
+      startTransition(() => setLanguage(routeLocale));
+    }
+  }, [routeLocale, language]);
 
   useEffect(() => {
     document.documentElement.lang = language;
